b5e9740994687c0d3d89007480b747bd04d72f04
Imam Fauzan <imamfzn@gmail.com>
2016-12-28 14:47:25 +0700

    register models to admin

--- kedai_app/admin.py
+++ kedai_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Gedung)
+admin.site.register(Pelanggan)
+admin.site.register(Pesanan)
+admin.site.register(Suplier)
+admin.site.register(Menu)
+admin.site.register(MenuHarian)
