56711ac3b276fab8976543face7453baf858415b
Imam Fauzan <imamfzn@gmail.com>
2016-12-28 15:25:18 +0700

    add urls route

---
+++ kedai_app/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url,include
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'pelanggan', PelangganViewSet)
+router.register(r'gedung', GedungViewSet)
+router.register(r'pesanan', PesananViewSet)
+#router.register(r'pesanan_customer', PesananCustomerViewSet,base_name='pesanan_customer')
+router.register(r'suplier', SuplierViewSet)
+router.register(r'menu', MenuViewSet)
+#router.register(r'menucustomer', MenuCustomerViewSet)
+router.register(r'menuharian', MenuHarianViewSet)
+
+
+urlpatterns = [
+    #url(r'^docs/', include('rest_framework_docs.urls')),
+]
+
+
+urlpatterns += router.urls
