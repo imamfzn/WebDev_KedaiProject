4a6d817f8e12a8a6a8cf69e5041a5ad0d4910995
Imam Fauzan <imamfzn@gmail.com>
2016-12-28 15:24:20 +0700

    add default view

--- kedai_app/views.py
+++ kedai_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class PelangganViewSet(viewsets.ModelViewSet):
+    queryset = Pelanggan.objects.all()
+    serializer_class = PelangganSerializer
+
+class GedungViewSet(viewsets.ModelViewSet):
+    queryset = Gedung.objects.all()
+    serializer_class = GedungSerializer
+
+class PesananViewSet(viewsets.ModelViewSet):
+    queryset = Pesanan.objects.all()
+    serializer_class = PesananSerializer
+
+class SuplierViewSet(viewsets.ModelViewSet):
+   
+    queryset = Suplier.objects.all()
+    serializer_class = SuplierSerializer
+
+class MenuViewSet(viewsets.ModelViewSet):
+  
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class MenuHarianViewSet(viewsets.ModelViewSet):
+    queryset = MenuHarian.objects.all()
+    serializer_class = MenuHarianSerializer
+
+class MenuCustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsReadOnly]
+    queryset = Menu.objects.all()
+    serializer_class = MenuCustomerSerializer
