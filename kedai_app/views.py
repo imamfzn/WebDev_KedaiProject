66c4d59fd3ef03506c81d8327714aac8fd522c0c
Imam Fauzan <imamfzn@gmail.com>
2017-01-05 21:47:36 +0700

    server time view

--- kedai_app/views.py
+++ kedai_app/views.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from .const import *
+import datetime
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+class ServerTime(APIView):
+    def get(self,request):
+        result = {
+            'date' : datetime.datetime.now().date(),
+            'time' : datetime.datetime.now().time()
+        }
+        return Response(result)
 
 class PelangganViewSet(viewsets.ModelViewSet):
     queryset = Pelanggan.objects.all()
@@ -26,5 +39,10 @@ class MenuViewSet(viewsets.ModelViewSet):
     serializer_class = MenuSerializer
 
 class MenuHarianViewSet(viewsets.ModelViewSet):
-    queryset = MenuHarian.objects.all()
-    serializer_class = MenuHarianSerializer
+    time_now = datetime.datetime.now().time()
+    if (time_now < MIN_TIME or time_now > MAX_TIME):
+        queryset = Menu.objects.all()
+        serializer_class = MenuSerializer
+    else:
+        queryset = MenuHarian.objects.all()
+        serializer_class = MenuHarianSerializer
