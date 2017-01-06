dd0b193eaba6576dfc01c86929abf967dd98d6e8
Imam Fauzan <imamfzn@gmail.com>
2017-01-06 09:46:40 +0700

    add permissions

--- kedai_app/permissions.py
+++ kedai_app/permissions.py
@@ -31,8 +31,17 @@ class OnlyCustomer(BasePermission):
 class OnlyCustomerService(BasePermission):
 	message = "You must logged in as customer"
 	def has_permission(self,request,view):
-		if request.user.is_authenticated():
+		if request.method in SAFE_METHODS:
 			return True
-		return False
+		return request.user.is_authenticated()
 
+class PesananPermission(BasePermission):
+	my_safe_methods = ['GET','POST']
+	def has_permission(self,request,view):
+		if not request.user.is_authenticated():
+			if request.method in self.my_safe_methods:
+				return True
+			else :
+				return False
+		else : return True
 
--- kedai_app/views.py
+++ kedai_app/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .models import *
 from .serializers import *
 from .const import *
+from .permissions import *
 import datetime
 
 
@@ -19,27 +21,34 @@ class ServerTime(APIView):
         return Response(result)
 
 class PelangganViewSet(viewsets.ModelViewSet):
+    permission_classes = [OnlyCustomer]
     queryset = Pelanggan.objects.all()
     serializer_class = PelangganSerializer
 
 class GedungViewSet(viewsets.ModelViewSet):
+    permission_classes = [OnlyCustomerService]
     queryset = Gedung.objects.all()
     serializer_class = GedungSerializer
 
 class PesananViewSet(viewsets.ModelViewSet):
+    permission_classes = [PesananPermission]
     queryset = Pesanan.objects.all()
     serializer_class = PesananSerializer
 
 class SuplierViewSet(viewsets.ModelViewSet):
+    permission_classes = [OnlyCustomerService]
     queryset = Suplier.objects.all()
     serializer_class = SuplierSerializer
 
 class MenuViewSet(viewsets.ModelViewSet):
+    permission_classes = [OnlyCustomerService]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class MenuHarianViewSet(viewsets.ModelViewSet):
+    permission_classes = [OnlyCustomerService]
     time_now = datetime.datetime.now().time()
+
     if (time_now < MIN_TIME or time_now > MAX_TIME):
         queryset = Menu.objects.all()
         serializer_class = MenuSerializer
